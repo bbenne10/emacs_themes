d406bf7b2205e04dbb91562e95a1dc421f2821be
Bryan Bennett <Bryan.Bennett@protonmail.com>
2023-01-25 13:24:36 -0500

    Allow autoloading each theme

---
+++ bennett-themes.el
@@ -0,0 +1,9 @@
+;;;###autoload
+(when (and (boundp 'custom-theme-load-path) load-file-name)
+  (let* ((base (file-name-directory load-file-name))
+         (dir (expand-file-name "themes/" base)))
+    (add-to-list 'custom-theme-load-path
+                 (or (and (file-directory-p dir) dir)
+                     base))))
+
+(provide 'bennett-themes)
