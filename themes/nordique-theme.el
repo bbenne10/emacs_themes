bdb21d4db6d19e0aed27b49b41c9a797c16c5e07
Bryan Bennett <Bryan.Bennett@protonmail.com>
2023-09-22 15:02:54 -0400

    Add everforest for modification (and some small changes to nordique)

--- themes/nordique-theme.el
+++ themes/nordique-theme.el
@@ -1,4 +1,7 @@
-(deftheme nordique "A bastardized lower contrast nord-inspired theme")
+;;; package --- Summary
+;;; Commentary:
+;;; Code:
+(deftheme nordique "A bastardized lower contrast nord-inspired theme.")
 (let* ((class '((class color) (min-colors 89)))
        (nord0 "#2E3440")
        (nord1 "#3B4252")
@@ -21,7 +24,9 @@
 
        (region "#3B4252")
        (string "#bec2cc")
-       (font-lock-keyword "#68758e"))
+       (font-lock-keyword "#68758e")
+       (hydra-pink "#bf6199"))
+
   (custom-theme-set-faces 'nordique
    `(default ((,class (:background ,nord0 :foreground ,nord4))))
    `(cursor ((,class (:background ,nord4 :inverse-video t))))
@@ -39,11 +44,16 @@
 
    `(git-gutter-fr:added ((,class (:foreground ,nord14))))
    `(git-gutter-fr:modified ((,class (:foreground ,nord15))))
-   `(git-gutter-fr:added ((,class (:foreground ,nord11))))
+   `(git-gutter-fr:deleted ((,class (:foreground ,nord11))))
+
+   `(diff-hl-insert ((,class (:foreground ,nord14 :background ,nord14))))
+   `(diff-hl-change ((,class (:foreground ,nord8 :background ,nord8))))
+   `(diff-hl-delete ((,class (:foreground ,nord11 :background ,nord11))))
 
    `(hl-line ((,class (:background ,highlight))))
    `(highlight ((,class (:background ,highlight))))
    `(match ((,class :background ,nord9)))
+   `(line-number ((,class (:background ,nord0 :foreground ,nord3 :slant italic))))
 
    ;; active region
    `(region ((,class (:background ,region))))
@@ -59,7 +69,7 @@
    `(font-lock-keyword-face ((,class (:foreground ,font-lock-keyword))))
    `(font-lock-string-face ((,class (:foreground ,nord4 :foreground ,string))))
    `(font-lock-type-face ((,class (:foreground ,nord15 :slant italic))))
-   `(font-lock-variable-name-face ((,class (:foreground ,nord4))))
+   `(font-lock-variable-name-face ((,class (:foreground ,nord4 :weight bold :slant italic))))
    `(font-lock-warning-face ((,class (:foreground ,nord11))))
 
    ;; ansi colors for vterm
@@ -77,20 +87,37 @@
    ;; mode-line
    `(mode-line ((,class . (:foreground ,nord4
                          :background ,nord1
-                         :inherit nil))))
-   `(mode-line-inactive ((,class . (:foreground ,comment
-                                  :background ,nord0))))
+                         :box nil))))
+   `(mode-line-inactive ((,class . (:foreground ,comment :background ,nord0 :box nil))))
    `(success ((,class . (:foreground ,nord14))))
    `(warning ((,class . (:foreground ,nord12))))
-   `(warning ((,class . (:foreground ,nord11))))
+   `(error ((,class . (:foreground ,nord11))))
+
+
+   `(tab-bar ((,class . nil)))
+   `(tab-bar-tab ((,class . (:background ,nord7 :foreground ,nord0 :box (:line-width 8 :color ,nord7)))))
 
    ;; fic mode
    `(fic-face  ((,class (:foreground ,nord12 :background ,nord0))))
 
+   ;; corfu
+   `(corfu-default ((,class (:foreground ,nord6 :background ,nord2))))
+   `(corfu-current ((,class (:foreground ,nord0 :background ,nord8))))
+
+   ;; hydra
+   `(hydra-face-amaranth ((,class (:foreground ,nord15))))
+   `(hydra-face-red ((,class (:foreground ,nord11))))
+   `(hydra-face-pink ((,class (:foreground ,hydra-pink))))
+   `(hydra-face-blue ((,class (:foreground ,nord10))))
+   `(hydra-face-teal ((,class (:foreground ,nord9))))
+
+   ;; git-gutter
+   `(git-gutter:added  ((,class (:foreground ,nord14))))
+   `(git-gutter:deleted ((,class (:foreground ,nord11))))
+   `(git-gutter:modified ((,class (:foreground ,hydra-pink))))
+   `(git-gutter:separator ((,class (:foreground ,nord8))))
    ))
 
-;;;###autoload
-(when (and (boundp 'custom-theme-load-path) load-file-name)
-  (add-to-list 'custom-theme-load-path
-               (file-name-as-directory (file-name-directory load-file-name))))
-(provide-theme 'nordique)
+(provide-theme 'bb-nordique)
+
+;;; nordique-theme.el ends here
